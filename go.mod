285f525b56e9863da04d0844949caf5e720a9b28
Joey Innes <joey@inneslabs.uk>
2024-04-01 16:48:34 +0200

    Make better

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/intob/qcp
 
 go 1.22.0
+
+require (
+	github.com/inneslabs/fnpool v0.0.2
+	github.com/inneslabs/jfmt v0.2.0
+)
--- main.go
+++ main.go
@@ -9,8 +9,17 @@ import (
 	"os/user"
 	"path"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync/atomic"
+
+	"github.com/inneslabs/fnpool"
+	"github.com/inneslabs/jfmt"
 )
+
+var ignoreExts = []string{
+	".DS_Store",
+}
 
 var inc = []string{
 	".gitconfig",
@@ -24,28 +33,42 @@ var inc = []string{
 	"Video/",
 }
 
-type job struct {
+type op struct {
 	src, dst string
-	do       func() <-chan error
+	do       func() <-chan *result
+}
+
+type result struct {
+	err error
+	n   int64
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		panic("specify src and dst paths")
+	skipConf := flag.Bool("y", false, "skip confirmation")
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		exit(1, "specify src and dst paths")
 	}
-	rootSrc, err := expandPath(os.Args[1])
+	rootSrc, err := expandPath(flag.Arg(0))
 	if err != nil {
-		panic(err)
+		exit(2, "err expanding src path: %v", err)
 	}
-	rootDst, err := expandPath(os.Args[2])
+	rootDst, err := expandPath(flag.Arg(1))
 	if err != nil {
-		panic(err)
+		exit(3, "err expanding dst path: %v", err)
 	}
 
 	match := func(src string) bool {
+		ext := path.Ext(src)
+		for _, ignoreExt := range ignoreExts {
+			if ext == ignoreExt {
+				return false
+			}
+		}
 		rel, err := filepath.Rel(rootSrc, src)
 		if err != nil {
-			panic(err)
+			exit(4, "err resolving path: %v", err)
 		}
 		for _, i := range inc {
 			if strings.HasPrefix(rel, i) {
@@ -55,20 +78,30 @@ func main() {
 		return false
 	}
 
-	skipConf := flag.Bool("y", false, "skip confirmation")
-	flag.Parse()
 	if !*skipConf && !confirm(rootSrc, rootDst, match) {
-		panic("aborted")
+		exit(4, "aborted by user")
 	}
 
+	pool := fnpool.NewPool(runtime.NumCPU())
+	var total atomic.Int64
 	for job := range walk(rootSrc, rootDst, match) {
-		err := <-job.do()
-		if err != nil {
-			fmt.Printf("ERROR: copy %s: %s", job.src, err)
-		} else {
-			fmt.Println("done:", job.src)
-		}
+		pool.Dispatch(func() {
+			res := <-job.do()
+			if res.err != nil {
+				fmt.Printf("ERROR: %v\n", res.err)
+				return
+			}
+			relDst, err := filepath.Rel(rootDst, job.dst)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("done: %s -> %s\n", flag.Arg(0), relDst)
+			total.Add(res.n)
+		})
 	}
+	pool.StopAndWait()
+	size := jfmt.FmtSize64(uint64(total.Load()))
+	fmt.Printf("copied %s to %s\n", size, rootDst)
 }
 
 func confirm(rootSrc, rootDst string, match func(src string) bool) bool {
@@ -81,8 +114,8 @@ func confirm(rootSrc, rootDst string, match func(src string) bool) bool {
 	return resp == "y"
 }
 
-func walk(rootSrc, rootDst string, match func(src string) bool) <-chan *job {
-	ops := make(chan *job, 1)
+func walk(rootSrc, rootDst string, match func(src string) bool) <-chan *op {
+	ops := make(chan *op, 1)
 	go func() {
 		defer close(ops)
 		filepath.WalkDir(rootSrc, func(src string, d fs.DirEntry, err error) error {
@@ -95,55 +128,52 @@ func walk(rootSrc, rootDst string, match func(src string) bool) <-chan *job {
 			if match(src) {
 				srcNoRoot := strings.Replace(src, rootSrc, "", 1)
 				dst := path.Join(rootDst, srcNoRoot)
-				ops <- &job{
+				ops <- &op{
 					src: src,
 					dst: dst,
 					do:  prepJob(src, dst),
 				}
 			}
-			return err
+			return nil
 		})
 	}()
 	return ops
 }
 
-func prepJob(src, dst string) func() <-chan error {
-	return func() <-chan error {
-		done := make(chan error)
+func prepJob(src, dst string) func() <-chan *result {
+	return func() <-chan *result {
+		done := make(chan *result)
 		go func() {
 			defer close(done)
 			rd, err := os.Open(src)
 			if err != nil {
-				done <- err
+				done <- &result{err, 0}
 				return
 			}
 			defer rd.Close()
 			info, err := os.Stat(src)
 			if err != nil {
-				done <- err
+				done <- &result{err, 0}
 				return
 			}
 			perm := info.Mode().Perm()
 			err = os.MkdirAll(path.Dir(dst), 0777)
 			if err != nil {
-				done <- err
+				done <- &result{err, 0}
 				return
 			}
 			wr, err := os.Create(dst)
 			if err != nil {
-				done <- err
+				done <- &result{err, 0}
 				return
 			}
 			defer wr.Close()
-			_, err = io.Copy(wr, rd)
+			n, err := io.Copy(wr, rd)
 			if err != nil {
-				done <- err
+				done <- &result{err, 0}
 				return
 			}
-			err = os.Chmod(dst, perm)
-			if err != nil {
-				done <- err
-			}
+			done <- &result{os.Chmod(dst, perm), n}
 		}()
 		return done
 	}
@@ -159,4 +189,9 @@ func expandPath(path string) (string, error) {
 		return filepath.Join(homeDir, path[2:]), nil
 	}
 	return filepath.Abs(path)
+}
+
+func exit(code int, msg string, args ...any) {
+	fmt.Printf(msg+"\n", args...)
+	os.Exit(code)
 }
